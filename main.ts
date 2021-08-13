f41e7b20718de1688bd81e17e077e128eb51d605
3lchicharit0 <88722345+3lchicharit0@users.noreply.github.com>
2021-08-13 09:09:34 -0500

    hice las horas

--- main.ts
+++ main.ts
@@ -1,18 +1,15 @@
-let HORA = 0
-let minutos = 0
 let segundos = 0
+let minutos = 58
+let HORA = 0
 basic.forever(function () {
     segundos += 1
     if (segundos == 59) {
-        segundos = 0
         minutos += 1
     }
     if (minutos == 59) {
-        minutos = 0
         HORA += 1
     }
-    if (HORA == 1) {
-        HORA = 0
-        HORA += 12
+    if (HORA == 12) {
+        HORA += 1
     }
 })
